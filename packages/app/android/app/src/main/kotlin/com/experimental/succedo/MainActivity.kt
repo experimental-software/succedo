d2389b6c145e0b9b5a788a71f839f737298cd3f2
Jan Mewes <jan.mewes@epages.com>
2020-11-06 22:59:01 +0100

    Add writing to cloud firestore

--- packages/app/android/app/src/main/kotlin/com/experimental/succedo/MainActivity.kt
+++ packages/app/android/app/src/main/kotlin/com/experimental/succedo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.app
+package com.experimental.succedo
 
 import io.flutter.embedding.android.FlutterActivity
 
--- packages/app/lib/add_note/add_note.dart
+++ packages/app/lib/add_note/add_note.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class AddNotePage extends StatelessWidget {
@@ -34,8 +36,12 @@ class AddNotePage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
+              onPressed: () async {
+                CollectionReference tasks = FirebaseFirestore.instance.collection('tasks');
+                tasks
+                    .add({"title": "${DateTime.now().toIso8601String()}", "details": "Hallo, Welt!", "position": null})
+                    .then((value) => print("Task added"))
+                    .catchError((error) => print("Failed to add task: $error"));
               },
               child: Text('Speichern'),
             ),
