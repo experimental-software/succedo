136d944800b6e70dd10afe3846d974cba285bc47
Jan Mewes <jan.mewes@experimental-software.com>
2021-01-01 16:56:42 +0100

    WIP  [ci skip]

--- packages/succedo_desktop/test/core/project_test.dart
+++ packages/succedo_desktop/test/core/project_test.dart
@@ -1,15 +1,17 @@
 // @dart=2.9
 import 'dart:io';
 
+import 'package:succedo_desktop/core/note.dart';
 import 'package:succedo_desktop/core/project.dart';
 import "package:test/test.dart";
+import 'package:uuid/uuid.dart';
 
 
 
 void main() {
   test("Should create new project", () async {
     final directory = await Directory.systemTemp.createTemp();
-    var path = "${directory.path}/subdir/example.xml"; // TODO What if there is a nonexistent subdirectory?
+    var path = "${directory.path}/subdir/example.xml";
     var name = "Test project";
     print(path);
 
@@ -18,6 +20,20 @@ void main() {
     expect(Project.current.name, equals(name));
     expect(Project.current.path, equals(path));
     expect(await File(path).exists(), isTrue);
+  });
+
+  test("Should save tasks in project", () async {
+    final directory = await Directory.systemTemp.createTemp();
+    var path = "${directory.path}/subdir/example.xml";
+    var name = "Test project";
+    Project.create(name: name, path: path);
+    Project.current.notes.add(Note(id: Uuid().v4(), title: "Test note"));
+
+    Project.current.save();
+
+    var fileContents = await File(path).readAsString();
+    print(fileContents);
+    expect(fileContents, isNotEmpty);
   });
 
   test("String.trim() removes surrounding whitespace", () {
