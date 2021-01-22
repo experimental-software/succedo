229c9d6dbf666cf0b8669d5328f0e5a3c5bb2e75
Jan Mewes <jan.mewes@experimental-software.com>
2021-01-22 07:12:19 +0100

    WIP  [ci skip]

--- packages/succedo_desktop/test/core/config_test.dart
+++ packages/succedo_desktop/test/core/config_test.dart
@@ -1,10 +1,7 @@
 // @dart=2.9
 import 'dart:io';
 
-import 'package:flutter/services.dart';
 import 'package:succedo_desktop/core/config.dart';
-import 'package:succedo_desktop/core/note.dart';
-import 'package:succedo_desktop/core/project.dart';
 import "package:test/test.dart";
 import 'package:uuid/uuid.dart';
 
@@ -21,19 +18,20 @@ void main() {
     expect(fileContents, contains(projectPath));
   });
 
-  test("Should load reference to last project", () async {
-    var lastProject = await Config.loadLastProject("test_resources/test_config.xml");
-    expect(lastProject, equals("test_resources/test_project_nested.xml"));
-  });
+  group("loadLastProject", () {
+    test("Should load reference to last project", () async {
+      var lastProject = await Config.loadLastProject("test_resources/test_config.xml");
+      expect(lastProject, equals("test_resources/test_project_nested.xml"));
+    });
 
-  // TODO use nested tests
-  test("Should handle no config option", () async {
-    var lastProject = await Config.loadLastProject("test_resources/empty_test_config.xml");
-    expect(lastProject, isNull);
-  });
+    test("Should handle no config option", () async {
+      var lastProject = await Config.loadLastProject("test_resources/empty_test_config.xml");
+      expect(lastProject, isNull);
+    });
 
-  test("Should handle no config file", () async {
-    var lastProject = await Config.loadLastProject("test_resources/${Uuid().v4()}.xml");
-    expect(lastProject, isNull);
+    test("Should handle no config file", () async {
+      var lastProject = await Config.loadLastProject("test_resources/${Uuid().v4()}.xml");
+      expect(lastProject, isNull);
+    });
   });
 }
